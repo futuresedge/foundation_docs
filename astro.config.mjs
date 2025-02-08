20df696c82cb44b46678d2157c8e43964f87eae4
pedrogrande <pete@peterargent.com>
2025-02-08 19:49:16 +1100

    home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,11 @@ import tailwind from "@astrojs/tailwind";
 // import { ion } from "starlight-ion-theme";
 import icon from "astro-icon";
 import { locate } from "@iconify/json";
+// import starlightBlog from "starlight-blog";
+import starlightHeadingBadges from "starlight-heading-badges";
+// import starlightThemeObsidian from "starlight-theme-obsidian";
+// import starlightSiteGraph from "starlight-site-graph";
+// import starlightSidebarTopics from "starlight-sidebar-topics";
 
 const huge = locate("hugeicons");
 
@@ -17,6 +22,37 @@ export default defineConfig({
     [icon()],
     starlight({
       title: "Future's Edge",
+      plugins: [
+        // starlightBlog(),
+        starlightHeadingBadges(),
+        // starlightThemeObsidian(),
+        // starlightSiteGraph(),
+        // starlightSidebarTopics([
+        //   {
+        //     label: "Documentation",
+        //     link: "/docs/",
+        //     icon: "open-book",
+        //     items: [
+        //       "foundation/foundation-introduction",
+        //       "foundation/vision",
+        //       "foundation/mission",
+        //       "foundation/values",
+        //       "governance/governance-introduction",
+        //       "governance/constitution",
+        //       "governance/councils",
+        //       "strategy/strategy-introduction",
+        //       "strategy/business-model-canvas",
+        //       "strategy/persona-profile",
+        //     ],
+        //   },
+        //     {
+        //       label: "Agent Handbook",
+        //       link: "/agent-handbook/",
+        //       icon: "information",
+        //       items: ["agent-handbook"],
+        //     },
+        //   ]),
+      ],
       sidebar: [
         { label: "Home", link: "/" },
         {
